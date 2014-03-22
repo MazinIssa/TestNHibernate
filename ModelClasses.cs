1b299ef45b5a9f22b86c9f401f16dc57db11bea5
MazinIssa <mazinissa@github.com>
2014-03-22 01:32:05 -0700

    Initial working commit
    After the experiments worked successfully and I was made happy.

---
+++ ModelClasses.cs
@@ -0,0 +1,80 @@
+﻿using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using FluentNHibernate.Conventions;
+using NHibernate.Tool.hbm2ddl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NHibernateTest.NHBNTypes
+{
+    public class Person
+    {
+        public virtual Guid Id { get; set; }
+        public virtual string Name { get; set; }
+        public virtual DateTime BirthDate { get; set; }
+        [Cascade]
+        public virtual IList<Item> Items { get; set; }
+    }
+
+    public class Item
+    {
+        public virtual Guid Id { get; set; }
+        public virtual string Name { get; set; }
+        public virtual int Price { get; set; }
+    }
+
+    public class DbConfig : FluentNHibernate.Automapping.DefaultAutomappingConfiguration
+    {
+        public override bool ShouldMap(Type type)
+        {
+            //return type.Namespace == "NHibernateTest.NHBNTypes";
+            return
+                type == typeof(Person)
+                || type == typeof(Item); //because of compiler generated classes.
+        }
+
+    }
+
+    public class CascadeConvention : IHasManyConvention, IReferenceConvention
+    {
+        public void Apply(FluentNHibernate.Conventions.Instances.IOneToManyCollectionInstance instance)
+        {
+            var attrs = instance.Member.GetCustomAttributes(typeof(CascadeAttribute), false);
+
+            if (attrs != null && attrs.Length != 0)
+                instance.Cascade.All();
+        }
+
+        public void Apply(FluentNHibernate.Conventions.Instances.IManyToOneInstance instance)
+        {
+            var attrs = instance.Property.MemberInfo.GetCustomAttributes(typeof(CascadeAttribute), false);
+
+            if (attrs != null && attrs.Length != 0)
+                instance.Cascade.All();
+        }
+    }
+
+    public class CascadeAttribute : Attribute { }
+
+    public static class nhbn
+    {
+        internal static NHibernate.ISessionFactory sessionFactory = null;
+        public static void does()
+        {
+            var connString = "Data Source=.\\SQLExpress;Initial Catalog=TestNHibernate;Integrated Security=SSPI;Trusted_Connection=Yes";
+            DbConfig d = new DbConfig();
+            var atpm = FluentNHibernate.Automapping.AutoMap
+                    .AssemblyOf<Item>(d)
+                    .Conventions.Add(typeof(CascadeConvention));
+
+            sessionFactory = Fluently.Configure()
+               .Database(MsSqlConfiguration.MsSql2008.ConnectionString(connString))
+               .ExposeConfiguration(cfg => new SchemaUpdate(cfg).Execute(false, true))
+               .Mappings(m => m.AutoMappings.Add(atpm))
+               .BuildSessionFactory();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentNHibernate;
+using FluentNHibernate.Automapping;
+using FluentNHibernate.Cfg;
+using NHibernateTest.NHBNTypes;
+
+namespace NHibernateTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //does does configure NHibernate. Excellent choice of a method name.
+            nhbn.does();
+
+            var session = nhbn.sessionFactory.OpenSession();
+
+            //SaveDataTest(session);
+            //GetDataTest(session);
+            UpdateExistingDataUsingUnAttachedObject(session);
+
+            session.Close();
+        }
+
+        private static void SaveDataTest(NHibernate.ISession session)
+        {
+            Person p = new Person() { Name = "lksjflkdsjf" };
+            Item i = new Item() { Name = "Existentialism", Price = 15 };
+            Item ii = new Item() { Name = "Artificial Intelligence", Price = 1 };
+            p.Items = new List<Item>();
+            p.Items.Add(i);
+
+            var transaction = session.BeginTransaction();
+
+            session.Save(p);
+
+            transaction.Commit();
+        }
+
+        private static void GetDataTest(NHibernate.ISession session)
+        {
+
+
+            var transaction = session.BeginTransaction();
+
+            var results1 = session.QueryOver<Person>().Where(x => x.Name == "Mazen").List();
+            var person = session.Get<Person>(new Guid("5A1E5ADC-9DC5-46B3-9AF6-A2F500B44CCF"));
+            person.Name = "Mazen";
+            person.BirthDate = DateTime.Now;
+            session.Save(person);
+
+            transaction.Commit();
+        }
+
+        private static void UpdateExistingDataUsingUnAttachedObject(NHibernate.ISession session)
+        {
+            Person p = new Person()
+            {
+                Name = "Mazen likes NHibernate",
+                BirthDate = new DateTime(1989, 5, 31),
+                Id = new Guid("5A1E5ADC-9DC5-46B3-9AF6-A2F500B44CCF")
+            };
+
+            var transaction = session.BeginTransaction();
+
+            //session.Save(p); //this ignored that the key is set to an existent object, and created a new row in the table!
+            session.SaveOrUpdate(p); //worked as expected. NHibernate is my ORM of choice from now on.
+
+            transaction.Commit();
+        }
+    }
+}
